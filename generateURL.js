0805d2909fc931ae3eaa5c4dfb99a3251c00f189
brighieleach <107080240+brighieleach@users.noreply.github.com>
2024-11-05 15:05:40 -0700

    Update generateURL.js

--- generateURL.js
+++ generateURL.js
@@ -2,5 +2,5 @@ const baseUrl = "https://app.mode.com/jasperai/reports/8770a4060686/embed"; // R
 const timestamp = Math.floor(Date.now() / 1000); // Current UNIX time in seconds
 const accessKey = "1d3b8f05ef4865d765500c95"; // Replace with the access key
 
-const signedUrl = `${baseUrl}?timestamp=${timestamp}&access_key=${accessKey}`;
+const signedUrl = `${baseUrl}?max_age=3600&access_key=${accessKey}`&timestamp=${timestamp};
 console.log(signedUrl);
